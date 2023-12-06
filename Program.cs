df2bb4539c33795406a990c8539036c58b5e376b
A252557 <A252557@LSBSA-3WCCHW3>
2023-12-06 14:10:24 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7.getting_user_input
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hi "+ name);
+            Console.WriteLine("how old are you?");
+            String age=Console.ReadLine();  
+            Console.WriteLine(name+" is "+  age+" years old");
+            Console.ReadLine();
+        }
+    }
+}
